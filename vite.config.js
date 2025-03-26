f92b61bb1bd30ef9331eb1510f1f75e762df6d19
Otavio Bigogno <otaviobigogno37@gmail.com>
2025-03-26 15:31:37 -0300

    img padrão

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { sveltekit } from "@sveltejs/kit/vite";
+import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
-export default {
+const config = {
   plugins: [sveltekit()],
+  server: {
+    allowedHosts: [
+      '5421-2804-30f4-106-fa00-696d-5175-da39-9379.ngrok-free.app',
+    ]
+  }
 };
+
+export default config;
