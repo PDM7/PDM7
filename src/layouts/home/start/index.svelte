daffc08a1545e8277999fe9f830c30826204a850
GustavoSilva84 <fogofracoo@gmail.com>
2025-04-27 16:23:25 -0300

    .

--- src/layouts/home/start/index.svelte
+++ src/layouts/home/start/index.svelte
@@ -1,8 +1,19 @@
-<main class="container" id="start">
-    <h1 id="title">Anamnese da ansiedade</h1>
-    <p id="description">Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a?</p>
-  
-    <button id="buttonStart">começar</button>
+<script>
+    import CardList from "../cardList/index.svelte";
+</script>
+
+<main id="start">
+    <div class="container">
+        <img id="blur" src="/home/start/blur.png" alt="">
+    
+        <h1 id="title">Anamnese da ansiedade</h1>
+        <p id="description">Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a?</p>
+    
+    
+        <button id="buttonStart">começar</button>
+    
+        <CardList/>
+    </div>
 </main>
 
 <style>
--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -2,13 +2,12 @@
 <script>
   import Nav from "../../layouts/home/nav/index.svelte";
   import Start from "../../layouts/home/start/index.svelte";
-  import CardList from "../../layouts/home/cardList/index.svelte";
 </script>
 
 <section>
   <Nav/>
   <Start/>
-  <CardList/>
+  
 </section>
 
 <style>
