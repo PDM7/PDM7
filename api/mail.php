7d9adcd195eb10bfdd12aa0d945299419fbbbb93
Otavio Bigogno <otaviobigogno37@gmail.com>
2025-05-09 00:50:04 -0300

    utf8

--- api/mail.php
+++ api/mail.php
@@ -26,6 +26,7 @@ loadEnv(__DIR__ . '/dados.env');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");  
 header("Access-Control-Allow-Headers: Content-Type"); 
+header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
